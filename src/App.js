defab3f1bab92ebb06c29754e3ad321321701cfc
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-24 11:29:16 +0400

    Settings for Tailwind content

--- src/App.js
+++
@@ -1,3 +0,0 @@
-export function App() {
-	return <h1 className="bg-blue-500">Hello world!</h1>;
-}
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Container from './components/Container'
+
+export function App() {
+	return (
+		<Container>
+			<h1 className="bg-blue-500">Hello world!</h1>
+		</Container>
+	)
+}
