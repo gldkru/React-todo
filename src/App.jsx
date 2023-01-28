61bcc26feb1f777924ebda460383f0fcd438a211
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-28 16:47:20 +0400

    feat(components): added classes in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import Container from './components/Container'
 export const App = () => {
   return (
     <Container>
-      <h1
-        className='
-        w-full
-        bg-blue-500
-        text-center
-      '
-      >
-        Hello world!
-      </h1>
+      <h1 className='w-full bg-blue-500 text-center'>Hello world!</h1>
     </Container>
   )
 }
