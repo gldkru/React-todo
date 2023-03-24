2a1244fa99af63932f487e84731b5280573e0810
Kirill Gladkikh <onest.kill@yandex.ru>
2023-03-24 19:08:25 +0400

    feat: React StrictMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { StrictMode } from 'react'
 import { Container } from './components/ui/Container'
 import { Todo } from './components/Todo'
 
 export const App = () => {
   return (
-    <Container>
-      <Todo />
-    </Container>
+    <StrictMode>
+      <Container>
+        <Todo />
+      </Container>
+    </StrictMode>
   )
 }
