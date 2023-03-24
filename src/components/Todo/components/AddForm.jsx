17f332ec7259145c038433a95076493bf1aea0f2
Kirill Gladkikh <onest.kill@yandex.ru>
2023-03-24 17:50:49 +0400

    feat(Todo): add task form

--- src/components/Todo/components/AddForm.jsx
+++ src/components/Todo/components/AddForm.jsx
@@ -1,15 +1,37 @@
+import { useContext, useState } from 'react'
 import { Button } from '../../ui/Button'
 import { PlusIcon } from '../../ui/icons/Plus'
+import { DispatchTasksContext } from '../context'
 
 export const AddForm = () => {
+  const [taskName, setTaskName] = useState('')
+  const dispatchTasks = useContext(DispatchTasksContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    dispatchTasks({
+      type: 'ADD_TASK',
+      taskName: taskName
+    })
+
+    setTaskName('')
+  }
+
   return (
-    <form action='#' className='flex h-12 w-full items-center overflow-hidden rounded-md border-2 bg-white pr-2'>
+    <form
+      action='#'
+      className='flex h-12 w-full items-center overflow-hidden rounded-md border-2 bg-white pr-2'
+      onSubmit={handleSubmit}
+    >
       <input
         type='text'
         className='h-full grow px-3 placeholder:text-gray-500 focus:outline-none'
         placeholder='Новая задача...'
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
       />
-      <Button className='bg-blue-600 text-white hover:bg-blue-500'>
+      <Button type='submit' className='bg-blue-600 text-white hover:bg-blue-500'>
         <PlusIcon />
       </Button>
     </form>
