b3c5306a317e421301de78907880b4cb8924e17d
Kirill Gladkikh <onest.kill@yandex.ru>
2023-03-24 17:51:37 +0400

    feat(Todo): api - added task id

--- src/components/Todo/api/createTask.js
+++ src/components/Todo/api/createTask.js
@@ -1,8 +1,11 @@
+import { v4 as uuid } from 'uuid'
+
 export const createTask = (taskName) => {
   return {
     // todo: time
     // todo: due time
     // todo: descrtiption
+    id: uuid(),
     name: taskName,
     completed: false
   }
