aa80c6c8291511c7835022c1c929342e5eec860f
Kirill Gladkikh <onest.kill@yandex.ru>
2023-03-24 17:28:13 +0400

    feat(Todo): reducers with api

--- src/components/Todo/context/reducers.js
+++ src/components/Todo/context/reducers.js
@@ -1,7 +1,37 @@
-// eslint-disable-next-line no-unused-vars
+import { addTask, createTask, removeTask, editTask, completeTask } from '../api'
+
 export const taskReducer = (state, action) => {
-  // todo: add task
-  // todo: remove task
-  // todo: edit task
-  // todo: complete task
+  switch (action.type) {
+    case 'ADD_TASK': {
+      const task = createTask(action.taskName)
+
+      return {
+        ...state,
+        list: addTask(state.list, task)
+      }
+    }
+
+    case 'REMOVE_TASK': {
+      return {
+        ...state,
+        list: removeTask(state.list, action.id)
+      }
+    }
+
+    case 'EDIT_TASK': {
+      const task = createTask(action.taskName)
+
+      return {
+        ...state,
+        list: editTask(state.list, task)
+      }
+    }
+
+    case 'COMPLETE_TASK': {
+      return {
+        ...state,
+        list: completeTask(state.list, action.id)
+      }
+    }
+  }
 }
