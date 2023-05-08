fa0d44e92ac802931b2c23a302ef313fcedec9a2
Kirill Gladkikh <onest.kill@yandex.ru>
2023-05-08 17:04:51 +0400

    build(custom): changed parcel to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/*.{html,js,jsx}', './src/components/**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
